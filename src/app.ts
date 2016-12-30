2cb61c3a2d3fae5eb373b67be11f7116f3d6c286
flareair <flareair0@gmail.com>
2016-12-30 16:30:40 +0300

    Add expand and collapse all buttons

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,13 @@ import { DataProvider } from './providers/dataProvider';
 
 $(document).ready(() => {
     let parent: JQuery = $('.container');
-    let accordion: Accordion = new Accordion(parent, new DataProvider());
+    let accordion: Accordion = new Accordion(parent, new DataProvider(), 'accordion1');
 
     accordion.setTitle('Filters');
     accordion.draw();
+
+    let secondAccordion: Accordion = new Accordion(parent, new DataProvider(), 'accordion2');
+
+    secondAccordion.setTitle('Widgets');
+    secondAccordion.draw();
 });
--- src/widgets/accordion/accordion.ts
+++ src/widgets/accordion/accordion.ts
@@ -8,19 +8,29 @@ let rawTemplate: string = require('./accordion.html');
 
 export default class Accordeon {
     private parent: JQuery;
+    private container: JQuery;
+    private containerId: string;
     private provider: DataProviderInterface;
     private title: string = 'default';
 
-    private elementClass: string = 'accordion__element';
-    private headingClass: string = 'accordion__element-heading';
-    private expandedElementClass: string = 'accordion__element_expanded';
+    public containerClass: string = 'accordion';
+    public elementClass: string = 'accordion__element';
+    public headingClass: string = 'accordion__element-heading';
+    public expandedElementClass: string = 'accordion__element_expanded';
+    public collapseAllClass: string = 'accordion__collapse-all';
+    public expandAllClass: string = 'accordion__expand-all';
 
-    constructor(parent: JQuery, provider: DataProviderInterface) {
+    constructor(parent: JQuery, provider: DataProviderInterface, id: string) {
         if (!parent[0]) {
             throw new TypeError('Parent element should be a valid DOM node');
         }
 
+        if (id.length <= 0 || $(`#${id}`)[0]) {
+            throw new TypeError('You should provide unique id');
+        }
+
         this.parent = parent;
+        this.containerId = id;
         this.provider = provider;
     }
 
@@ -28,7 +38,10 @@ export default class Accordeon {
         let data = this.getData();
 
         this.parent.append(this.compileTemplate(rawTemplate, data));
-        this.setEventHandlers();
+        this.container = this.parent.find(`#${this.containerId}`);
+
+        let headings = this.getHeadings();
+        this.setEventHandlers(headings);
     }
 
     public setTitle(title: string): void {
@@ -42,14 +55,25 @@ export default class Accordeon {
     private compileTemplate(rawTemplate: string, data: AccordionItem[]): string {
         return template(rawTemplate)({
             title: this.title,
+            id: this.containerId,
             data
         });
     }
 
-    private setEventHandlers(): void {
+    private setEventHandlers(headings: JQuery): void {
 
-        this.parent.find(`.${this.headingClass}`).click((event) => {
+        headings.click((event) => {
             this.toggleElement(event);
+        });
+
+        this.container.find(`.${this.collapseAllClass}`).click((event: JQueryInputEventObject) => {
+            event.preventDefault();
+            this.collapseAll(headings);
+        });
+
+        this.container.find(`.${this.expandAllClass}`).click((event: JQueryInputEventObject) => {
+            event.preventDefault();
+            this.expandAll(headings);
         });
     }
 
@@ -57,5 +81,17 @@ export default class Accordeon {
         let element: JQuery = $(event.target);
 
         element.parents(`.${this.elementClass}`).toggleClass(this.expandedElementClass);
+    }
+
+    private getHeadings(): JQuery {
+        return this.container.find(`.${this.headingClass}`);
+    }
+
+    private collapseAll(headings: JQuery): void {
+        headings.parent().removeClass(this.expandedElementClass);
+    }
+
+    private expandAll(headings: JQuery): void {
+        headings.parent().addClass(this.expandedElementClass);
     }
 }
